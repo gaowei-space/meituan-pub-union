5eb3d70a22d1e48f5d1f962f3d4aa8c07b0d0d55
高伟 <gaowei@tech.sapce>
2021-11-21 22:18:49 +0800

    feat: add provider for laravel

---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the gaowei-space/weather.
+ *
+ * (c) gaowei <huyao9950@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace GaoweiSpace\MeituanPubUnion;
+
+use GaoweiSpace\MeituanPubUnion\Http\Client;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
+    {
+        $this->app->singleton(Client::class, function () {
+            return new Client(config('services.meituan_pub_union.app_key'), config('services.meituan_pub_union.utm_source'));
+        });
+        $this->app->alias(Client::class, 'MeituanPubUnion');
+    }
+
+    public function provides()
+    {
+        return [Client::class, 'MeituanPubUnion'];
+    }
+}
